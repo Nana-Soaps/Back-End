e6fb0ee0cfc351c15797714a5d0ed1adb205e698
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-10-12 08:36:48 -0400

    added field to shipping options (description)

--- api/data/migrations/20211008153642_shipping_options.js
+++ api/data/migrations/20211008153642_shipping_options.js
@@ -2,6 +2,7 @@ exports.up = async function (knex) {
   await knex.schema.createTable("shipping_options", (tbl) => {
     tbl.increments("shipping_id");
     tbl.string("shipping_name").notNullable();
+    tbl.string("shipping_description");
     tbl.float("cost").unsigned().notNullable();
   });
 };
--- api/data/seeds/05 - shipping_options.js
+++ api/data/seeds/05 - shipping_options.js
@@ -1,7 +1,19 @@
 const shipping_options = [
-  { shipping_name: "Standard Shipping", cost: 4.5 },
-  { shipping_name: "Priority Shipping", cost: 9.5 },
-  { shipping_name: "2-Day Shipping", cost: 15.0 },
+  {
+    shipping_name: "Standard Shipping",
+    shipping_description: "4-7 Business Days",
+    cost: 4.5,
+  },
+  {
+    shipping_name: "Priority Shipping",
+    shipping_description: "3-5 Business Days",
+    cost: 9.5,
+  },
+  {
+    shipping_name: "2-Day Shipping",
+    shipping_description: "2 Business Days",
+    cost: 15.0,
+  },
 ];
 
 exports.shipping_options = shipping_options;
