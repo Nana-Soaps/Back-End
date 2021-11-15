d61510651ec9af87dbfe5b4d7b278a62f65a2a06
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-11-14 20:42:16 -0500

    setup initial api call with mailchimp

--- api/emails/emails-router.js
+++ api/emails/emails-router.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const mailchimpTx = require("@mailchimp/mailchimp_transactional")(
+  process.env.MAILCHIMP_API
+);
 
 router.get("/", async (req, res, next) => {
   try {
-    res.status(200).json({ message: "works" });
+    const response = await mailchimpTx.users.ping();
+    res.status(200).json(response);
   } catch (err) {
     next(err);
   }
