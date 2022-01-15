99f155ec92c58ab2afa2c9532240f12d4baa0384
Marat Kotik <mkotik@Marats-MacBook-Pro.local>
2022-01-15 14:21:29 -0500

    added email

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const productsRouter = require("./products/products-router");
 const ordersRouter = require("./orders/orders-router");
 const emailsRouter = require("./emails/emails-router");
