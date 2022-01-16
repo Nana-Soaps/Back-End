bbb22228885e111df5f720e97317d0744158171a
Marat Kotik <mkotik97@gmail.com>
2022-01-15 20:22:53 -0500

    converted cors to *

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const ordersRouter = require("./orders/orders-router");
 const emailsRouter = require("./emails/emails-router");
 
 const corsOptions = {
-  origin: "http://localhost:3001",
+  origin: "*",
   credentials: true,
   optionSuccessStatus: 200,
 };
